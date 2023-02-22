44ce5f0a8271523c9bd2e027e288fb37de7d16f2
raffamartins4 <84045317+raffamartins4@users.noreply.github.com>
2023-02-22 20:33:02 -0300

    envio de tarefa como o codigo calculoMedia

---
+++ CalculoMedia/src/main/java/com/br/rmartins/CalculoMedia.java
@@ -0,0 +1,24 @@
+package com.br.rmartins;
+
+public class CalculoMedia {
+
+	public static void main(String[] args) {
+
+       int nota1 = 8;
+       
+       int nota2 = 6;
+       
+       int nota3 = 8;
+       
+       int nota4 = 7;
+       
+       int n = nota1 + nota2 + nota3 + nota4;
+       
+       double media = n / 4.0;
+       
+       System.out.println(n);
+       System.out.println(media);
+    		   
+	}
+
+}
